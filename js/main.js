e3fc05be2cbf11da64636ba9cc30b4b667d327f4
Keratry <nicolas.keratry@gmail.com>
2023-10-27 10:07:45 +0600

    fix: navbar

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
-document.querySelectorAll('li span').forEach(function (link) {
+/*document.querySelectorAll('li span').forEach(function (link) {
     link.addEventListener('click', function () {
         this.parentElement.classList.toggle('opened');
     });
-});
+});*/
+const navbar = document.querySelector('.navbar');
+window.addEventListener('scroll', () => {
+    if (this.scrollY > 1) {
+        navbar.classList.add('navbar-light');
+    } else {
+        navbar.classList.remove('navbar-light');
+    }
+})
