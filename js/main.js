ab107fb5ebb4d9a2814f5d973122587b12523199
Keratry <nicolas.keratry@gmail.com>
2023-10-28 20:20:23 +0600

    fix sprite svg

--- js/main.js
+++ js/main.js
@@ -4,10 +4,13 @@
     });
 });*/
 const navbar = document.querySelector('.navbar');
+const logo = document.querySelector('.logo use');
 window.addEventListener('scroll', () => {
     if (this.scrollY > 1) {
         navbar.classList.add('navbar-light');
+        logo.href.baseVal = './img/sprite.svg#logo';
     } else {
         navbar.classList.remove('navbar-light');
+        logo.href.baseVal = './img/sprite.svg#logo-light';
     }
 })
