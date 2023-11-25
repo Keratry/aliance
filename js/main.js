54ba18c18a671f691b920d85a1717128fdac30cf
Keratry <nicolas.keratry@gmail.com>
2023-11-25 16:44:50 +0600

    mobile menu

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@
 });*/
 const navbar = document.querySelector('.navbar');
 const logo = document.querySelector('.logo use');
+const mMenuToggle = document.querySelector('.mobile-menu-toggle');
+const menu = document.querySelector('.mobile-menu');
+
+
 window.addEventListener('scroll', () => {
     if (this.scrollY > 1) {
         navbar.classList.add('navbar-light');
@@ -13,4 +17,9 @@ window.addEventListener('scroll', () => {
         navbar.classList.remove('navbar-light');
         logo.href.baseVal = './img/sprite.svg#logo-light';
     }
-})
+});
+
+mMenuToggle.addEventListener('click', (event) => {
+    event.preventDefault();
+    menu.classList.toggle('is-open');
+});
