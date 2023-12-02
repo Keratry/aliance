12b06f1f6d21cb3395b97feb523df10b6185dd5b
Keratry <nicolas.keratry@gmail.com>
2023-12-02 17:58:35 +0600

    modal

--- js/main.js
+++ js/main.js
@@ -116,3 +116,30 @@ const swiperBlog = new Swiper('.blog-slider', {
         }
     }
 });
+
+
+const modal = document.querySelector('.modal');
+const modalDialog = document.querySelector('.modal-dialog');
+
+document.addEventListener('click', (event) => {
+    if (event.target.dataset.toggle == 'modal' ||
+        event.target.parentNode.dataset.toggle == 'modal' ||
+        (!event.composedPath().includes(modalDialog) && modal.classList.contains('is-open'))
+    ) {
+        modal.classList.toggle('is-open');
+    }
+
+});
+
+document.addEventListener('keyup', (event) => {
+    if (event.key == 'Escape' && modal.classList.contains('is-open')) {
+        modal.classList.toggle('is-open');
+    }
+});
+
+// const modalToggle = document.querySelectorAll('[data-toggle=modal]');
+// modalToggle.forEach((element) => {
+//     element.addEventListener('click', (event) => {
+//         modal.classList.add('is-open');
+//     })
+// });
